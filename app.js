b5f29cbbbc1faef5d1eb1b64758970c538f28183
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-16 17:49:00 +0200

    Nodes

--- app.js
+++ app.js
@@ -36,13 +36,14 @@ Array.from(titles).forEach(function (item) {
 const wrap = document.querySelector('#wrapper');
 console.log(wrap);
 
-const wmf = document.querySelector('.book-list li:nth-child(2) .name');
+let wmf;
+wmf = document.querySelector('#book-list li:nth-child(2) .name');
 console.log (wmf);
 
-let books = document.querySelector('.book-list li .name');
+let books = document.querySelector('#book-list li .name');
 console.log (books);
 
-books = document.querySelectorAll('.book-list li .name');
+books = document.querySelectorAll('#book-list li .name');
 console.log (books);
 
 Array.from(books).forEach(function (book) {
@@ -56,6 +57,20 @@ Array.from(books).forEach(function (book) {
      book.textContent += ' (Book title)';
 });
 
-const bookList = document.querySelector('.book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
+ let bookList = document.querySelector('#book-list');
+ //bookList.innerHTML = '<h2>Books and more books...</h2>';
  bookList.innerHTML += '<p>This is how you add HTML content</p>';
+
+ //6. Nodes
+let banner = document.querySelector('#page-banner');
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
+//7. DOM TRAVERSING (PARENT/CHILD) PART1
+
+
+
+

e863ac8b91339dd4e10f349b567e81fb896ef18a
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-16 08:33:19 +0200

    add comments

--- app.js
+++ app.js
@@ -24,3 +24,14 @@ console.log (books);
 Array.from(books).forEach(function (book) {
     console.log(book);
 });
+
+//5.Changing text & HTML content
+const books1 = document.querySelectorAll('.book-list li .name');
+
+Array.from(books1).forEach(function (book) {
+    return book.textContent += ' (Book title)';
+});
+
+const bookList = document.querySelector('.book-list');
+//bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
